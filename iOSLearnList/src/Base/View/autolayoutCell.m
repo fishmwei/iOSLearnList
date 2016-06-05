8c05abbd0ca5762bdb8ea592985a612cf28811fe
fishmwei <fishmwei@qq.com>
2016-06-05 17:34:03 +0800

    自动布局 uitableviewCell

---
+++ iOSLearnList/src/Base/View/autolayoutCell.m
@@ -0,0 +1,24 @@
+//
+//  autolayoutCell.m
+//  iOSLearnList
+//
+//  Created by mingwei on 16/6/5.
+//  Copyright © 2016年 fishmwei. All rights reserved.
+//
+
+#import "autolayoutCell.h"
+
+@implementation autolayoutCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
