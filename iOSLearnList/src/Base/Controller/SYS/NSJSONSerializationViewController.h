6a5d2be76853183297156e401814c833040c9be8
fishmwei <fishmwei@qq.com>
2016-10-25 21:05:05 +0800

    NSJSONSerialization

---
+++ iOSLearnList/src/Base/Controller/SYS/NSJSONSerializationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSJSONSerializationViewController.h
+//  iOSLearnList
+//
+//  Created by mingwei on 2016/10/24.
+//  Copyright © 2016年 fishmwei. All rights reserved.
+//
+
+#import "MWBaseViewController.h"
+
+@interface NSJSONSerializationViewController : MWBaseViewController
+
+@end
---
+++ iOSLearnList/src/Base/Controller/SYS/NSJSONSerializationViewController.m
@@ -0,0 +1,61 @@
+//
+//  NSJSONSerializationViewController.m
+//  iOSLearnList
+//
+//  Created by mingwei on 2016/10/24.
+//  Copyright © 2016年 fishmwei. All rights reserved.
+//
+
+#import "NSJSONSerializationViewController.h"
+
+@interface NSJSONSerializationViewController () {
+    dispatch_queue_t dispatchQueue;
+    UITextView *showView;
+}
+@end
+
+@implementation NSJSONSerializationViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    [self setupData];
+    [self setupUI];
+    
+}
+
+- (void)setupData {
+    dispatchQueue = dispatch_queue_create(NSStringFromClass([self class]).UTF8String, DISPATCH_QUEUE_CONCURRENT);
+}
+
+- (void)setupUI {
+    
+    showView = [[UITextView alloc] initWithFrame:CGRectMake(0, 100, 200, 300)];
+    [self.view addSubview:showView];
+    
+    dispatch_async(dispatchQueue, ^{
+        NSURL *url = [NSURL URLWithString:@"https://api.douban.com/v2/movie/subject/25881786"];
+        NSURLRequest *req = [[NSURLRequest alloc] initWithURL:url];
+        NSURLSession *session = [NSURLSession sharedSession];
+        NSURLSessionDataTask *task = [session dataTaskWithRequest:req completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            NSDictionary *resp = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+            
+                showView.text = [NSString stringWithFormat:@"%@", resp];
+            });
+            
+        }];
+        [task resume];
+    });
+    
+    
+    
+}
+
+- (void)showAppend:(NSString *)str {
+    NSString *text = showView.text;
+    showView.text = [text stringByAppendingString:[NSString stringWithFormat:@"%@\n", str]];
+    
+}
+@end
