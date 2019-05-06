7b0ff68ba7faf9e87bc7bf00cf3cab5f1243c3e5
mingwei <huangmingwei88@qq.com>
2019-05-06 08:54:49 +0800

    【解决问题】: hook 功能 【修改内容】: 【提交人】： KEEP

---
+++ iOSLearnList/src/Test/MWBaseViewController_hook.h
@@ -0,0 +1,17 @@
+//
+//  MWBaseViewController+MWBaseViewController_hook.h
+//  iOSLearnList
+//
+//  Created by mingwei on 2019/5/6.
+//  Copyright © 2019年 fishmwei. All rights reserved.
+//
+
+#import "MWBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MWBaseViewController (MWBaseViewController_hook)
+
+@end
+
+NS_ASSUME_NONNULL_END
